4d3607f3ebff6acaaeb76d3356a28aa9ddae23a0
Nazlı ÖZBEK <you@example.com>
2022-03-31 23:05:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace proje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Veri Tipleri
+            byte b=5;
+            sbyte c=5;
+            short s=5;
+            ushort us =5;
+            
+            Int16 i16=2;
+            int i=2;
+            Int32 i32=2;
+            Int64 i64=2;
+            
+            uint ui=2;
+            long l=2;
+            ulong ul=4;
+            
+            float f=5;
+            double d=5;
+            decimal de=5;
+
+            char ch='2';
+            string str="David";
+
+            bool b1=true;
+            bool b2=false;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1="x";
+            object o2='y';
+            object o3=4;
+            object o4=4.3;
+
+            string str1= string.Empty;
+            str1="David Beckham";
+            string ad="David";
+            string soyad="Beckham";
+            string tamIsim= ad + "" + soyad;
+
+            int integer1=5;
+            int integer2=3;
+            int integer3=integer1 * integer2;
+
+            bool bool1 = 10<2;
+
+            string str20="20";
+            int int20=20;
+            string yenideger=str20 + int20.ToString();
+            Console.WriteLine(yenideger); // cikti 2020
+
+            int int21= int20 + Convert.ToInt32(str20);
+            Console.WriteLine(int21); // cikti 40
+
+            int int22 =int20+ int.Parse(str20);
+
+            string datetime= DateTime.Now.ToString("dd.MM,yyyy");
+            Console.WriteLine(datetime);
+            string datetime2= DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(datetime2);
+            string hour= DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(hour);
+
+
+
+
+
+
+
+        }
+    }
+}
